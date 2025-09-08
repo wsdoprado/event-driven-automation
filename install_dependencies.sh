327a0ee4ce1f8738adf0829b6726b9b089a78289
William Prado <79108556+wsdoprado@users.noreply.github.com>
2025-09-08 14:25:48 -0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -17,6 +17,9 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+sudo apt install -y curl
+sudo apt install -y wget
+
 # uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 export PATH=$PATH:/root/.local/bin
@@ -33,8 +36,6 @@ sudo apt install -y docker-ce
 sudo apt install -y docker-ce-cli 
 sudo apt install -y containerd.io 
 sudo apt install -y docker-compose-plugin
-sudo apt install -y curl
-sudo apt install -y wget
 
 docker --version
 docker compose version
