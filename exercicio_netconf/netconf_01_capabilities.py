89b9d6d3a15222f8c6e46f91708c816966ac83c7
William Prado <79108556+wsdoprado@users.noreply.github.com>
2025-09-08 15:48:55 -0300

    Update netconf_01_capabilities.py

--- exercicio_netconf/netconf_01_capabilities.py
+++ exercicio_netconf/netconf_01_capabilities.py
@@ -17,18 +17,8 @@ DEVICE = {
     "hostkey_verify": False,
 }
 
+
 with manager.connect(**DEVICE) as m:
-    filter_xml = """
-    <filter>
-      <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
-        <interface/>
-      </interfaces>
-    </filter>
-    """
-    interfaces = m.get(filter=filter_xml)
-
-# Parse e formata
-xml_parsed = minidom.parseString(interfaces.xml)
-pretty_xml = xml_parsed.toprettyxml(indent="  ")  # Indentação com 2 espaços
-
-print(pretty_xml)
+    print("=== Capabilities do dispositivo ===")
+    for cap in m.server_capabilities:
+        print(cap)
